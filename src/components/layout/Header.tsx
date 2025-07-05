5eb795da918dcde65b526740bd9fcafefd08a815
nhatran22 <85084209+nhatran22@users.noreply.github.com>
2025-07-05 19:32:43 +0700

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -19,14 +19,6 @@ const socials = [
 ]
 
 export const Header: React.FC = () => {
-    const [scrolled, setScrolled] = React.useState(false)
-
-    React.useEffect(() => {
-        const onScroll = () => setScrolled(window.scrollY > 8)
-        window.addEventListener("scroll", onScroll)
-        return () => window.removeEventListener("scroll", onScroll)
-    }, [])
-
     return (
         <header
             className={cn(
